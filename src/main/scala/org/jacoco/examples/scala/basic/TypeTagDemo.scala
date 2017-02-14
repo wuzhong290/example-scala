f8a8052caeec6ddc2db13ab6ed9cb0e48fc2774b
wuzhong <419479707@qq.com>
2017-02-14 13:47:53 +0800

    泛型

--- src/main/scala/org/jacoco/examples/scala/basic/TypeTagDemo.scala
+++ src/main/scala/org/jacoco/examples/scala/basic/TypeTagDemo.scala
@@ -9,11 +9,15 @@ class Foo{
 
 object TypeTagDemo {
   def m2(f: Foo)(b: f.Bar)(implicit ev: TypeTag[f.Bar]) = ev
-
   def main(args: Array[String]) {
     val f1 = new Foo
     val b1 = new f1.Bar
-    val ev3 = m2(f1)(b1)
-    println(ev3.tpe)
+    val ev21 = m2(f1)(b1)
+    println(ev21.tpe)
+    val f2 = new Foo
+    val b2 = new f2.Bar
+    val ev22 = m2(f2)(b2)
+    println(ev22.tpe)
+    println(ev21==ev22)
   }
 }
