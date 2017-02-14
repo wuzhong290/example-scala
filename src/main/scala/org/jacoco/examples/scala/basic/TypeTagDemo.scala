b88663fd45c663320966f399db417c5e8d56e1c0
wuzhong <419479707@qq.com>
2017-02-14 16:56:18 +0800

    泛型

--- src/main/scala/org/jacoco/examples/scala/basic/TypeTagDemo.scala
+++ src/main/scala/org/jacoco/examples/scala/basic/TypeTagDemo.scala
@@ -6,7 +6,12 @@ import scala.reflect.runtime.universe._
 class Foo{
   class Bar
 }
-
+class Pair[T : Ordering](val first: T, val second: T) {
+  // 1. 编译后，Pair里面会有一个隐式的Ordering[T]的字段
+  // 2. 定义一个带有隐式参数的方法，调用方法时，会自动获取Pair类的隐式字段Ordering[T]
+  def smaller(implicit ord: Ordering[T]) =
+    if (ord.compare(first, second) < 0 ) first else second
+}
 object TypeTagDemo {
   def m2(f: Foo)(b: f.Bar)(implicit ev: TypeTag[f.Bar]) = ev
   def main(args: Array[String]) {
@@ -19,5 +24,7 @@ object TypeTagDemo {
     val ev22 = m2(f2)(b2)
     println(ev22.tpe)
     println(ev21==ev22)
+    val p = new Pair(100,20)
+    println(p.smaller)
   }
 }
